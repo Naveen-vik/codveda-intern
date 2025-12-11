e6f5e14ec9431045bcf59e9632140716632c5360
Naveen-vik <naveenvikram2003@gmail.com>
2025-12-11 10:36:55 +0530

    version3

---
+++ wordcount.py
@@ -0,0 +1,16 @@
+def count_words(filename):
+    try:
+        with open("wordsin.txt", "r") as file:
+            content = file.read()
+            words = content.split()
+            print("File Contents:\n")
+            print(content)
+            print("\nTotal Words:", len(words))
+
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+
+
+count_words("countword.txt")
